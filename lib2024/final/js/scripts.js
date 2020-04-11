14c3f71b6c7b3a3f052d409fffb493b2456e6d2c
ande0506 <59573420+ande0506@users.noreply.github.com>
2020-04-11 12:21:10 -0400

    java does not work

--- lib2024/final/js/scripts.js
+++ lib2024/final/js/scripts.js
@@ -2,32 +2,23 @@ window.onload = init;
 
 function init() {
 
-	$('#form-container').submit(function (e) {
-   		 e.preventDefault();
-   		 var form = this; 
-   		 $(".overlay-container").fadeIn(1000, function(){
-   		 		$('.overlay-container').delay(500).fadeOut(500);
-   		 })
-	});
+// JQuery function attached to the submit event of the form with id "form"
+$('#form').submit(function (e) {
+	// e.preventDefualt to avoid the form being submitted to page specified in action attribute 
+   	 e.preventDefault();
+   	// passing the current form (this) to variable form 
+   	$("#results").removeClass('reveal');
+   	var form = this;
+   	// fadeIn is a jQuery function to fadeIn an element 
+   	$(".overlay-container").fadeIn(1000, function(){
+			   		//call the showFormValues function and pass variable form to it as argument
+			   		showformValues(form);
+					// fadeOut is a jQuery function to fadeOut an element 
+			   		$('.overlay-container').delay(500).fadeOut(500);
+			   		$("#results").addClass('reveal');
+   	})
+});
 
-}
-
-	// JQuery function attached to the submit event of the form with id "form"
-	$('#form').submit(function (e) {
-		// e.preventDefualt to avoid the form being submitted to page specified in action attribute 
-   		 e.preventDefault();
-   		 // passing the current form (this) to variable form 
-   		 $("#results").removeClass('reveal');
-   		 var form = this;
-   		 // fadeIn is a jQuery function to fadeIn an element 
-   		 $(".overlay-container").fadeIn(1000, function(){
-   		 		//call the showFormValues function and pass variable form to it as argument
-   		 		showformValues(form);
-		   		 // fadeOut is a jQuery function to fadeOut an element 
-   		 		$('.overlay-container').delay(500).fadeOut(500);
-   		 		$("#results").addClass('reveal');
-   		 })
-	});
 
 	// to show the form values in the results div which takes the argument "form"
 	function showformValues(form){
@@ -45,9 +36,20 @@ function init() {
 			// 3) Modifies the text inside the selected element and replaces it with the value of this field   
 			$("#results").find("#"+name+"_result").text(field.value);
 
+			
+
 			// special check for email to add a link instead of just string
 			if(field.name=="email"){
 				$("#results").find("#"+email+"_result").attr("href", "mailto:"+field.value);
+
+			// $("#results").find("#"+address+"_result").text(field.value);
+
+			// $("#results").find("#"+city+"_result").text(field.value);
+
+			// $("#results").find("#"+province+"_result").text(field.value);
+
+			// $("#results").find("#"+pc+"_result").text(field.value);
+
 			}
 		})				
 	}
